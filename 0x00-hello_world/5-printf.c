5222619b6ccf2b3603a672b556df2515fcbde10a
solomon594 <antirisolomon594@gmail.com>
2022-07-02 16:03:09 -0700

    My commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
 #!/bin/bash
+#include <stdio.h>                                                                                         
+
+/**                                                                                                        
+ * main-entry point          
+ *
+ * Return:0 after print
+ */                           
+int main(void)    
+{
+	printf("with proper grammer, but outcome is a piece of art,\n");
+	return(0);
+}
