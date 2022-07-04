f10a1ee7db92b8262fddf3ad12d964501c39eb12
solomon594 <antirisolomon594@gmail.com>
2022-07-04 11:05:06 -0700

    My commit

--- 0x01-variables_if_else_while/4-print_alphabet.c
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -1,2 +1,17 @@
-#!/bin/bash
-
+#inclde<stdio.h>
+/**
+ * main -entry point
+ * Return:always return 0
+ */
+int main(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a = 'q' || a = 'e')
+		{
+			continue;
+		}
+		putchar (a);
+		return (0)
+	}
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - entry point
+ * Retuurn: always return 0
+ */
+int main(void)
+{
+	char a;
+	for (a = "a"; a >= "a"; a++)
+		putchar(a);
+	putchar("\n");
+	return (0);
+}
