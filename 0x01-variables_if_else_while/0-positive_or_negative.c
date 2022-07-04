4c4d29d1092b216c68afec700740686a3580c517
solomon594 <antirisolomon594@gmail.com>
2022-07-04 13:20:08 -0700

    My commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ n = rand() - RAND_MAX / 2;
 
 if (n < 0)
 {
-	printf("% d is negative\n", n);
+	printf("%d is negative\n", n);
 }
 else if (n == 0)
 {
@@ -22,7 +22,7 @@ else if (n == 0)
 }
 else
 {
-printf("% d is positive\n", n); 
+printf("%d is positive\n", n); 
 }
 return (0);
 }
