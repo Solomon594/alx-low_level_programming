c36b16c8214b1de1034855c31a9aca1b5c5191e6
solomon594 <antirisolomon594@gmail.com>
2022-07-04 20:47:06 -0700

    My commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-#!/bin/bash
-#include <stdio.h>                                                                                         
-
-/**                                                                                                        
- * main-entry point          
+#include <stdio.h>
+/*
+ * main-entry point
  *
  * Return:0 after print
- */                           
-int main(void)    
+ */
+int main(void)
 {
-	printf("with proper grammer, but outcome is a piece of art,\n");
-	return(0);
+printf("with proper grammer, but outcome is a piece of art,\n");
+return (0);
 }
