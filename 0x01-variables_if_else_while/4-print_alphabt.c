76c059e9988e63dfa8e1cfcb45ca85ba1ceb7f6e
Solomon594 <antirisolomon594@gmail.com>
2022-10-15 06:08:16 -0700

    second commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,10 +6,14 @@
  */
 int main(void)
 {
-	char alphabet
-		for(alphabet = 'a' ; alphabet <= 'z' ; alphabet ++) {
-			putchar(alphabet);
+	char alphabet;
+
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			if (alphabet != 'e' && alphabet != 'q')
+				putchar(alphabet);
 		}
-	putchar("\n");
+
+	putchar('\n');
 	return (0);
 }
