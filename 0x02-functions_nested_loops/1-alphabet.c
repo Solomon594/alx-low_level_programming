a006ec67bff794387a85da715f2a50b1de7f11d6
Solomon594 <antirisolomon594@gmail.com>
2022-10-18 21:23:21 -0700

    second c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,13 @@
 
 /**
  * print_alphabet - fuction will print alphabets
- * Return: alway 0
  */
 void print_alphabet(void);
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
-	{
 		_putchar(c);
-	}
 
 	_putchar('\n');
 	return (0);
