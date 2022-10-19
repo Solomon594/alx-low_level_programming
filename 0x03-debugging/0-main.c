7346ae06ae1937df357afb8984c218965b1ec6b3
Solomon594 <antirisolomon594@gmail.com>
2022-10-19 00:39:35 -0700

    fourth commit fourth c low

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test if a function that prints integer is + or -ve
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
