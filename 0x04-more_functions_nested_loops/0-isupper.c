d75d8fbecfc93cc059a43e7c6bc634b493b1d989
Solomon594 <antirisolomon594@gmail.com>
2022-10-20 04:43:02 -0700

    fifth commit c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a letter is upper
+ * @c: the letter
+ * Return: 1 for upper else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 and c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
