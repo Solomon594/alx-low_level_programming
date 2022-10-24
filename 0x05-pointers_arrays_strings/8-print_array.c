181fda3bb54b34fa4a1062e089160b30f7987cd6
Solomon594 <antirisolomon594@gmail.com>
2022-10-24 05:04:51 -0700

    fifth c commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n element of an array
+ * @a: array
+ * @n: number of values
+ */
+void print_array(int *a, int n)
+{
+	int y;
+
+	for (y = 0; y < n; y++)
+	{
+		printf("%d", a[y]);
+		if (y != n - 1)
+			printf(", ");
+	}
+}
