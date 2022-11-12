c21cb74e62c3474d50fece6c1509504bb392cb8a
Solomon594 <antirisolomon594@gmail.com>
2022-11-11 18:49:19 -0800

    more malloc free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,28 +13,30 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size1 = 0, size2 = 0, i;
 
 	if (size1 == NULL)
-		s1 = ' ';
+		s1 = " ";
+
 	if (size2 == NULL)
-		s2 = ' ';
+		s2 = " ";
+
 	while (s1[size1] != '\0')
 	{
 		size1++;
 	}
-	if (s2[size2] != '\0')
+	while (s2[size2] != '\0')
 	{
 		size2++;
 	}
 	if (n > size2)
 	{
 		n = size2;
-		p = malloc((size1 + n + 1) * (sizeof(char));
-				}
-				if (p == NULL)
-				{
-				return (0);
-				}
-				for (i = 0; i < size1; i++)
-			p[i] =s1[i];
+	p = malloc((size1 + n + 1) * (sizeof(char));
+			}
+			if (p == NULL)
+			{
+			return (0);
+			}
+			for (i = 0; i < size1; i++)
+			p[i] = s1[i];
 
 
 			for (; i < (size1 + n); i++)
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,5 +11,4 @@ int *array_range(int min, int max);
 int _putchar(char c);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-
 #endif
