f9b362d0a3b22938b65fba815d127ae8f19a2c1b
Solomon594 <antirisolomon594@gmail.com>
2022-11-16 20:04:54 -0800

    pointer function

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calc sum of ints
+ * @a: first int
+ * @b: srcond
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calc diff
+ * @a: sirst
+ * @b: secong
+ * Return: diff
+ */
+int op_sub(int a, int b)
+{
+	return (a  b);
+}
+
+/**
+ * op_mul - calc prod of ints
+ * @a: first
+ * @b: second
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: first
+ * @b: second
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calc nodulus
+ * @a: first
+ * @b: second
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
