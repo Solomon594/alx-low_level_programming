8c8ad7dbece6bba79f4aa03322218ae087e008ac
Solomon594 <antirisolomon594@gmail.com>
2023-01-16 11:50:18 -0800

    dynamic memory

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_atoi - ato
+ *@s: string to convert
+ * Return: string to int
+ */
+
+int _atoi(char *s)                                                
+{
+	*s = 'i';
+}               
